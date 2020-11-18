c42365859a541cf3ee99b0ceb489b1e8d116493f
lizza <liutao_glory@126.com>
2020-11-18 20:24:11 +0800

    lizza commit.

--- java-concurrency/src/main/java/com/lizza/ThreadCommunication/WaitNotify_2.java
+++ java-concurrency/src/main/java/com/lizza/ThreadCommunication/WaitNotify_2.java
@@ -1,49 +1,64 @@
 package com.lizza.ThreadCommunication;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 题目: 4个线程，交替打印A、B、C，循环10次; 使用等待通知机制实现
  *
- * 思路: 使用synchronized + wait/notify + 状态量实现
+ * 思路: 使用Lock + await/signal + Condition + 状态量实现
  */
-public class WaitNotify_1 {
+public class WaitNotify_2 {
 
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
     private String flag = "A";
 
     private void printA() {
-        synchronized (this) {
-            while (flag != "A") try { this.wait(); } catch (Exception e) {}
+        lock.lock();
+        try {
+            while (flag != "A") condition.await();
             System.out.println(Thread.currentThread().getName() + ": A");
             this.flag = "B";
-            this.notifyAll();
-        }
+            condition.signalAll();
+        } catch (Exception e) {} finally { lock.unlock(); }
+
     }
 
     private void printB() {
-        synchronized (this) {
-            while (flag != "B") try { this.wait(); } catch (Exception e) {}
+        lock.lock();
+        try {
+            while (flag != "B") condition.await();
             System.out.println(Thread.currentThread().getName() + ": B");
             this.flag = "C";
-            this.notifyAll();
-        }
+            condition.signalAll();
+        } catch (Exception e) {} finally { lock.unlock(); }
+
     }
 
     private void printC() {
-        synchronized (this) {
-            while (flag != "C") try { this.wait(); } catch (Exception e) {}
+        lock.lock();
+        try {
+            while (flag != "C") condition.await();
             System.out.println(Thread.currentThread().getName() + ": C");
             this.flag = "A";
-            this.notifyAll();
-        }
+            condition.signalAll();
+        } catch (Exception e) {} finally { lock.unlock(); }
+
     }
 
     public static void main(String[] args){
-        WaitNotify_1 object = new WaitNotify_1();
+        WaitNotify_2 object = new WaitNotify_2();
+
         new Thread(() -> {
             for (int i = 0; i < 10; i++) object.printA();
         }, "t1").start();
+
         new Thread(() -> {
             for (int i = 0; i < 10; i++) object.printB();
         }, "t2").start();
+
         new Thread(() -> {
             for (int i = 0; i < 10; i++) object.printC();
         }, "t3").start();
