f81c81367100b77c579fd103087b7492f57dd94a
lizza <liutao_glory@126.com>
2020-12-18 09:20:40 +0800

    lizza commit.

--- java-concurrency/src/main/java/com/lizza/ConcurrentHashMap/ConcurrentHashMap_1.java
+++ java-concurrency/src/main/java/com/lizza/ConcurrentHashMap/ConcurrentHashMap_1.java
@@ -3,17 +3,14 @@ package com.lizza.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * @author lt
- * @date 2019年5月27日
- * @version v1.0
+ * ConcurrentHashMap 源码学习
+ *
  */
 public class ConcurrentHashMap_1 {
 
 	public static void main(String[] args) {
 		ConcurrentHashMap<String, Object> map = new ConcurrentHashMap<>();
+		// put 方法
 		map.put("name", "张三");
-		map.put("age", 15);
-		System.out.println(map);
-		map.get("name");
 	}
 }
