03523613fa13f7cc0744548caffe127a7b013269
lizza <liutao_glory@126.com>
2021-01-01 11:07:58 +0800

    lizza commit.

--- java-concurrency/src/main/java/com/lizza/ConcurrentHashMap/ConcurrentHashMap_1.java
+++ java-concurrency/src/main/java/com/lizza/ConcurrentHashMap/ConcurrentHashMap_1.java
@@ -1,5 +1,6 @@
 package com.lizza.ConcurrentHashMap;
 
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -9,8 +10,38 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConcurrentHashMap_1 {
 
 	public static void main(String[] args) {
-		ConcurrentHashMap<String, Object> map = new ConcurrentHashMap<>();
+		ConcurrentHashMap<Object, Object> map = new ConcurrentHashMap<>(64);
 		// put 方法
-		map.put("name", "张三");
+		User[] users = new User[10];
+		for (int i = 0; i < 10; i++) {
+			User user = new User(i);
+			users[i] = user;
+			map.put(user, "用户-" + i);
+		}
+		System.out.println(map.get(users[1]));
+	}
+}
+
+class User {
+	int id;
+	public User(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean equals(Object o) {
+		return false;
+	}
+	public int hashCode() {
+		return 0;
+	}
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				'}';
 	}
 }
